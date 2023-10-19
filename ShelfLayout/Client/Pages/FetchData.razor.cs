cd08dd8e196ce67c8c11e4a87699f2eeb83cdf52
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2023-10-19 22:28:41 +0900

    implement codebehind

---
+++ ShelfLayout/Client/Pages/FetchData.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using ShelfLayout.Shared;
+
+namespace ShelfLayout.Client.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+        }
+    }
+}
